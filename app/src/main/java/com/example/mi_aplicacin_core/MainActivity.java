380ff05146900160c79ada2ae48bbbb0efb2549b
Alita <alejandramufandy14@gmail.com>
2022-06-19 22:17:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mi_aplicacin_core/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mi_aplicacin_core;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView booksingTextView = findViewById(R.id.booksingTextView);
+        ImageView LogoBooksing = findViewById(R.id.LogoBooksing);
+
+        booksingTextView.setAnimation(animacion2);
+        LogoBooksing.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        } ,400);
+    }
+}
